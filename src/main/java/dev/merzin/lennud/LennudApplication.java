ea3995528c3ef900b7e525b5fd1396016c4534b1
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-03-07 19:04:22 +0200

    spring initializr

---
+++ src/main/java/dev/merzin/lennud/LennudApplication.java
@@ -0,0 +1,13 @@
+package dev.merzin.lennud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LennudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LennudApplication.class, args);
+	}
+
+}
