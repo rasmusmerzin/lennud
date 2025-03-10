f40c13b553534f915a22f59c5d638ba0786d7979
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-03-10 17:18:43 +0200

    frontend: change adapter fallback

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ fallback: "index.html" }),
+		adapter: adapter({ fallback: "404.html" }),
 		paths: { base: "/lennud" },
 	},
 };
